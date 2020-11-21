1276aa87315d0b58650e19c2c2a97ae9e4c09e68
dbusato <davidedbusato@gmail.com>
2020-11-21 12:31:26 +0100

    Starting...again

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../cgraphics/src/app/app.h"
+#include "../X11-C-Graphics/src/app/app.h"
 
 static app_t *app;
 
@@ -10,44 +10,50 @@ static void update(int64_t elapsed_time)
 
 static void draw(void)
 {
-    app_clear_screen(app);
-    app_show_fps(app, 0, 0);
+	app_clear_screen(app);
+	app_show_fps(app, 0, 0);
 }
 
 static void key_event(int key, int action)
 {
-    if (action == KEY_PRESS) {
-        switch (key) {
-            case KEY_ESC: {
-                app_stop(app);
-                break;
-            }
-            case KEY_F1: {
-                app_toggle_fullscreen(app);
-                break;
-            }
-        }
-    }
+	if (action == KEY_PRESS) {
+		switch (key)
+		{
+		case KEY_ESC:
+		{
+			app_stop(app);
+			break;
+		}
+		case KEY_F1:
+		{
+			app_toggle_fullscreen(app);
+			break;
+		}
+		}
+	}
 }
 
 static void finish(void)
 {
 }
 
-int main() {
-    screen_settings_t *screen_settings = malloc(sizeof(screen_settings));
-    screen_settings->window_title = "Ray Casting";
-    screen_settings->width = 1366;
-    screen_settings->height = 768;
-    screen_settings->targetFps = 60;
-    screen_settings->show_stats = true;
-    screen_settings->fullscreen = false;
-    app = app_init(screen_settings);
-    app_callbacks_t *cbs = malloc(sizeof(app_callbacks_t));
-    cbs->key_fun = key_event;
-    cbs->update = update;
-    cbs->draw = draw;
-    cbs->finish = finish;
-    cbs->print_final_stats = NULL;
-    app_run(app, cbs);
+int main()
+{
+	screen_settings_t *screen_settings = malloc(sizeof(screen_settings_t));
+	screen_settings->window_title = "Ray Casting";
+	screen_settings->width = 1366;
+	screen_settings->height = 768;
+	screen_settings->targetFps = 60;
+	screen_settings->show_stats = true;
+	screen_settings->fullscreen = false;
+	app = app_init(screen_settings);
+	app_callbacks_t *cbs = malloc(sizeof(app_callbacks_t));
+	cbs->key_fun = key_event;
+	cbs->update = update;
+	cbs->draw = draw;
+	cbs->finish = finish;
+	cbs->print_final_stats = NULL;
+	app_run(app, cbs);
+	free(screen_settings);
+	free(cbs);
 }
